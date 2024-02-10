ecc7c0f836d515792070066b0c97f5c748bcd11d
jaffar <keikei.jaffar@mail.utoronto.ca>
2024-02-10 05:35:41 -0500

    Correct validation logic for email input

---
+++ loginRenderer.js
@@ -0,0 +1,6 @@
+console.log('loginRenderer script is loaded');
+
+document.getElementById('loginBtn').addEventListener('click', function() {
+    // validate the login credentials here
+    window.location.href = 'report.html';
+});
